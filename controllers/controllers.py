cbf48620f16a42b1ba41a46696c5da2f61b12714
Gildas Tema <gildastema3@gmail.com>
2020-04-13 12:22:15 +0100

    second commit

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class Demo-backend(http.Controller):
-#     @http.route('/demo-backend/demo-backend/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Demo-backend(http.Controller):
+    @http.route('/demo-backend/demo-backend/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/demo-backend/demo-backend/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('demo-backend.listing', {
-#             'root': '/demo-backend/demo-backend',
-#             'objects': http.request.env['demo-backend.demo-backend'].search([]),
-#         })
+    @http.route('/demo-backend/demo-backend/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('demo-backend.listing', {
+            'root': '/demo-backend/demo-backend',
+            'objects': http.request.env['demo-backend.demo-backend'].search([]),
+        })
 
-#     @http.route('/demo-backend/demo-backend/objects/<model("demo-backend.demo-backend"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('demo-backend.object', {
-#             'object': obj
-#         })
+    @http.route('/demo-backend/demo-backend/objects/<model("demo-backend.demo-backend"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('demo-backend.object', {
+            'object': obj
+        })
--- models/models.py
+++ models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-# class demo-backend(models.Model):
-#     _name = 'demo-backend.demo-backend'
+class demo-backend(models.Model):
+    _name = 'demo-backend.demo-backend'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
